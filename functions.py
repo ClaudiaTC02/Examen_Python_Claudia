81fd676cc71963a5e6bdd63f8847e6dd7d18a364
Claudia <claudiatcruz2002@gmail.com>
2023-03-22 10:45:46 +0100

    commit_3

--- functions.py
+++ functions.py
@@ -1,13 +1,21 @@
 import csv
 def read_data(fichero):
     diccionario = dict()
+    titulos = []
     with open(fichero, 'r') as file:
         reader = csv.reader(file, delimiter=',')
         indice = 1
+        fila = ""
         for line in reader:
+            titulos = line[0]
             for valor in line:
                 if len(valor) == 0:
-                    print("Vacio")
-            #diccionario.update({"dato"+str(indice) : line})
-            indice+=1
+                    fila = line
+            if(fila != line and indice != 1):
+                diccionario_nuevo = dict()
+                for i in titulos:
+                    diccionario_nuevo.update({i: str(line[i])})
+                    print(diccionario_nuevo)
+                diccionario.update({"dato"+str(indice) : diccionario_nuevo})
+                indice+=1
     return diccionario
